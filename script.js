dbedb82a2007d56dde952bc6807968cf966c98fa
Пустой <88878911+PystoyPlayer@users.noreply.github.com>
2024-06-21 02:03:46 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,63 +1,65 @@
 var url = "https://api.minetools.eu/ping/57.129.13.63/30046"; 
- $.getJSON(url, function(r) {
+$.getJSON(url, function(r) {
  if(r.error){
     $('#rest').html('Server Offline');
-   return false;
+    return false;
  } 
-  $('#rest').html('<b>Game:</b> '+r.description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
+ var description = r.description.replace('§R', '');
+ $('#rest').html('<b>Game:</b> '+description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
  $('#favicon').attr('src', r.favicon);  
 });
 
 var url2 = "https://api.minetools.eu/ping/5.188.141.184/25571"; 
 $.getJSON(url2, function(r) {
-    //data is the JSON string
  if(r.error){
     $('#rest2').html('Server Offline');
-   return false;
+    return false;
  } 
-  $('#rest2').html('<b>Game:</b> '+r.description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
+ var description = r.description.replace('§R', '');
+ $('#rest2').html('<b>Game:</b> '+description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
  $('#favicon2').attr('src', r.favicon);
 }); 
 
 var url3 = "https://api.minetools.eu/ping/142.132.223.37/25652"; 
 $.getJSON(url3, function(r) {
-    //data is the JSON string
  if(r.error){
     $('#rest3').html('Server Offline');
-   return false;
+    return false;
  } 
-  $('#rest3').html('<b>Game:</b> '+r.description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
+ var description = r.description.replace('§R', '');
+ $('#rest3').html('<b>Game:</b> '+description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
  $('#favicon3').attr('src', r.favicon);
 });
 
 var url4 = "https://api.minetools.eu/ping/168.119.145.214/25869"; 
- $.getJSON(url4, function(r) {
+$.getJSON(url4, function(r) {
  if(r.error){
     $('#rest4').html('Server Offline');
-   return false;
+    return false;
  } 
-  $('#rest4').html('<b>Game:</b> '+r.description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
+ var description = r.description.replace('§R', '');
+ $('#rest4').html('<b>Game:</b> '+description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
  $('#favicon4').attr('src', r.favicon);  
 });
 
 var url5 = "https://api.minetools.eu/ping/"; 
 $.getJSON(url5, function(r) {
-    //data is the JSON string
  if(r.error){
     $('#rest5').html('Server Offline');
-   return false;
+    return false;
  } 
-  $('#rest5').html('<b>Game:</b> '+r.description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
+ var description = r.description.replace('§R', '');
+ $('#rest5').html('<b>Game:</b> '+description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
  $('#favicon5').attr('src', r.favicon);
 }); 
 
 var url6 = "https://api.minetools.eu/ping/"; 
 $.getJSON(url6, function(r) {
-    //data is the JSON string
  if(r.error){
     $('#rest6').html('Server Offline');
-   return false;
+    return false;
  } 
-  $('#rest6').html('<b>Game:</b> '+r.description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
+ var description = r.description.replace('§R', '');
+ $('#rest6').html('<b>Game:</b> '+description+'<br><b>Online:</b> '+r.players.online+'/'+r.players.max);
  $('#favicon6').attr('src', r.favicon);
 });
